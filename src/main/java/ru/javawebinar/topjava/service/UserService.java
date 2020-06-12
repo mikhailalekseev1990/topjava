003546b9f882bdd8165733b7199740a30005393e
mikhail <mikhailalekseev1990@gmail.com>
2020-06-12 16:24:17 +0300

    annotation processing

--- src/main/java/ru/javawebinar/topjava/service/UserService.java
+++ src/main/java/ru/javawebinar/topjava/service/UserService.java
@@ -1,5 +1,8 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
@@ -8,9 +11,14 @@ import java.util.List;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFound;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 
+@Service
 public class UserService {
 
-    private UserRepository repository;
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
 
     public User create(User user) {
         return repository.save(user);
