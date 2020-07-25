417f5147e74085cf244ccc38e28f971df15b5469
mikhail <mikhailalekseev1990@gmail.com>
2020-07-25 12:44:29 +0300

    Implementation MealRestController and MealRestControllerTest without MealRestControllerTest.getBetween.

--- src/main/java/ru/javawebinar/topjava/to/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/to/MealTo.java
@@ -3,18 +3,21 @@ package ru.javawebinar.topjava.to;
 import java.time.LocalDateTime;
 
 public class MealTo {
-    private final Integer id;
+    private  Integer id;
 
-    private final LocalDateTime dateTime;
+    private  LocalDateTime dateTime;
 
-    private final String description;
+    private  String description;
 
-    private final int calories;
+    private  int calories;
 
 //    private final AtomicBoolean excess;      // filteredByAtomic
 //    private final Boolean excess;            // filteredByReflection
 //    private final Supplier<Boolean> excess;  // filteredByClosure
-    private final boolean excess;
+    private  boolean excess;
+
+    public MealTo() {
+    }
 
     public MealTo(Integer id, LocalDateTime dateTime, String description, int calories, boolean excess) {
         this.id = id;
